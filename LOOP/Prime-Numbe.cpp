4bd2fc76e18b39a40ccc459883adba200b0f4e41
Ujjal Burnwal <ujjwalbur1@gmail.com>
2025-11-09 14:29:23 +0530

    Implement code changes to enhance functionality and improve performance

---
+++ LOOP/Prime-Numbe.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    long long num;
+    int i,j;
+
+    cout<<"Enter the number: ";
+    cin>>num;
+
+    for(j=1;j<=num;j++){
+        bool flag=true;
+        for(i=2;i<j;i++){
+            if(j%i==0){
+                flag=false;
+            }
+        }
+        if(flag==true)
+            cout<<"Prime Number"<<"  " <<j <<endl ;
+        
+    }
+//     for(i=2;i<num;i++){
+//         if(num%i==0){
+//             flag=false;
+//         }
+//     }
+//     if(flag==true)
+//         cout<<"Prime Number";
+//     else
+//         cout<<"Not an Prime Number";
+}
