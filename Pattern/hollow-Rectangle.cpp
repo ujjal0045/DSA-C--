0ca5e2402726e6994f695208a55216060db8bd11
Ujjal Burnwal <ujjwalbur1@gmail.com>
2025-11-11 23:16:33 +0530

    Implement code changes to enhance functionality and improve performance

---
+++ Pattern/hollow-Rectangle.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n=4;
+    for(int i=1;i<=n;i++){
+        cout<<"x";
+        for(int j=1;j<=n-1;j++){
+            if(i == 1 || i == n){
+                cout<<"x";
+            } else{
+                cout<<" ";
+            }
+        }
+        cout<<"x" <<endl;
+    }
+}
