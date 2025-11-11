bde572a01b4134b5800b7766cbac34787c177b5c
Ujjal Burnwal <ujjwalbur1@gmail.com>
2025-11-11 15:44:12 +0530

    Implement code changes to enhance functionality and improve performance

---
+++ Pattern/Inverted-Star-Pattern3.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i,j;
+    for(i=4;i>=1;i--){
+        for(j=i;j>=1;j--){
+            cout<<"* " <<" ";
+        }
+        cout<<endl;
+    }
+}
