ed4ec211aca3c3d0f7e8c00a4e926f2b39ad91bc
Ujjal Burnwal <ujjwalbur1@gmail.com>
2025-11-13 00:10:17 +0530

    Implement code changes to enhance functionality and improve performance

---
+++ LOOP/Happy-Number.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main(){
+    long num,sum;
+    int r;
+    cout<<"Enter the number: ";
+    cin>>num;
+
+    while(num > 9){
+        while(num != 0){
+            r = num % 10;
+            sum+= r* r;
+            num/=10;
+        }
+        num = sum;
+        sum = 0;
+    }
+    if(num == 1){
+        cout<<"Happy Number";
+    } else {
+        cout<<"Not an happy numeber";
+    }
+}
