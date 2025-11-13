03ce8945eee0f3eb940de423fbf1530c344cc2a1
Ujjal Burnwal <ujjwalbur1@gmail.com>
2025-11-13 14:38:43 +0530

    Implement code changes to enhance functionality and improve performance

---
+++ Function/Prime-Number.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+int isPrime(int num){
+    bool flag=true;
+    for(int i=2;i*i<=num;i++){
+        if(num % i == 0){
+            return false;
+            break;
+        }
+    }
+    return true;
+}
+
+int main()
+{
+    int num;
+    cout<<"Enter the number: ";
+    cin>>num;
+
+    if(isPrime(num)){
+        cout<<"Prime Number";
+    } else{
+        cout<<"Not an Prime Number";
+    }
+    return 0;
+}
