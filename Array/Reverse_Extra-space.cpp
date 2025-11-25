6d4b18af34cb98d11c387ceb531a042993f63636
Ujjal Burnwal <ujjwalbur1@gmail.com>
2025-11-25 22:12:14 +0530

    Reverse with extra space

---
+++ Array/Reverse_Extra-space.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+void printArr(int arr[],int n){
+    for(int i=0;i<n;i++){
+        cout<< arr[i] <<" "; 
+    }
+    cout<<endl;
+}
+
+int main(){
+    int arr[] = {5,4,3,2,1};
+    int n = sizeof(arr) / sizeof(int);
+
+    int copyArr[n];
+    for(int i=0;i<n;i++){
+        int j = n-i-1;
+        copyArr[i] = arr[j];
+    }
+    for(int i=0;i<n;i++){
+        arr[i] = copyArr[i];
+    }
+    printArr(arr,n);
+}
