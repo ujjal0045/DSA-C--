56725ccb0854d3ec1a2361ec9e7a442240716e5a
Ujjal Burnwal <ujjwalbur1@gmail.com>
2025-11-26 22:13:54 +0530

    Binary Search

---
+++ Array/Binary-Search.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+int binarySearch(int arr[],int n,int key){
+    int start = 0,mid=0,end=n-1;
+    while(start<=end){
+        mid = (start+end) / 2;
+        if(arr[mid] == key){
+            return mid;
+        }
+        else if(arr[mid] < key){
+            start = mid+1;
+        }
+        else{
+            end = mid-1;
+        }
+    }
+    return -1;
+}
+
+int main(){
+    int arr[] = {2,4,6,8,10,12,14,16};
+    int n = sizeof(arr) / sizeof(int);
+    int key;
+    cout<<"Enter the search value : ";
+    cin>>key;
+    cout<< binarySearch(arr,n,key) <<endl; 
+}
