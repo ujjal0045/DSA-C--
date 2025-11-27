146c7f68df3e6dde4c41e771404da45e45f618e8
Ujjal Burnwal <ujjwalbur1@gmail.com>
2025-11-28 01:09:34 +0530

    Learning how to count trapped water using array

---
+++ Array/trapping-Rainwater.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+
+void trappedWater(int bar[],int n){
+    int leftMax[20000],rightMax[20000];
+    leftMax[0] = bar[0];
+
+    // Here we find all left bar
+    for(int i=1;i<n;i++){
+        leftMax[i] = max(leftMax[i-1],bar[i-1]);
+        
+    }
+    
+
+    // Here we find all right bar
+    rightMax[n-1] = bar[n-1]; 
+    for(int i=n-2;i>=0;i--){
+        rightMax[i] = max(rightMax[i+1],bar[i+1]);
+        
+    }
+
+    // Water trapped
+    int waterTrapped = 0;
+    for(int i=0;i<n;i++){
+        int curWater = min(leftMax[i],rightMax[i]) - bar[i];
+        if(curWater > 0) {
+            waterTrapped+= curWater;
+        }
+    }
+    cout<<"Total water trapped : "<<waterTrapped <<endl;
+    // return waterTrapped;
+}
+
+int main(){
+    int bar[] = {4,2,0,6,3,2,5};
+    int n = sizeof(bar) / sizeof(int);
+    trappedWater(bar,n);
+}
