58a730194e66f9c9eb71d673e01338e1e5821b66
Ujjal Burnwal <ujjwalbur1@gmail.com>
2025-11-29 14:31:32 +0530

    using function to enchance the code and easy to read

--- Array/Sorting/Bubble-Sort.cpp
+++ Array/Sorting/Bubble-Sort.cpp
@@ -6,19 +6,31 @@ void printArr(int nums[],int n){
     for(int i=0;i<n;i++){
         cout<<nums[i] <<" ";
     }
+    cout<<endl;
 }
 
-int main() {
-    int arr []= {5,4,1,3,2};
-    int n = sizeof(arr) / sizeof(int);
+void bubbleSort(int arr[],int n){
     // Bubble Sorting 
+    bool isSwap = false;
     for(int i=0;i<n-1;i++){
         for(int j=0;j<n-i-1;j++){
             if(arr[j] > arr[j+1]){
                 swap(arr[j] , arr[j+1]);
+                isSwap= true;
             }
+        }
+        if(!isSwap){
+            cout<<"It is already sorted..." <<endl;
+            printArr(arr,n);
+            return;
         }
     }
     printArr(arr,n);
+}
+
+int main() {
+    int arr []= {1,2,3,4,5};
+    int n = sizeof(arr) / sizeof(int);
+    bubbleSort(arr,n);
     return 0;
 }
