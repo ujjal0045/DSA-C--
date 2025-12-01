23e372127bea6826177982e27152fdbacf8e3546
Ujjal Burnwal <ujjwalbur1@gmail.com>
2025-12-02 01:14:36 +0530

    Learn Diagonal sum

---
+++ Array/2D Array/Diagonal-Sum.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+void diagonalSum(int arr[][3],int r,int c){
+    int SumR = 0 ,SumL = 0,common = 0;
+    //Left
+    for(int i=0,j=c-1;i<r;i++){
+        if( i == j) common = arr[i][j];
+        SumR+=arr[i][i];
+        SumL+= arr[i][j];
+        j--;
+    }
+    cout<<"Right sum : "<<SumR <<endl;
+    cout<<"Left Sum : "<<SumL <<endl;
+    int sum = (SumL + SumR) - common;
+    cout<<"Diagonal Sum is =  "<<sum <<endl;
+    
+}
+
+int main(){
+    int arr[4][4] = {{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};
+    // diagonalSum(arr,4,4);
+    int arr2[3][3] = {{1,2,3},{4,5,6},{7,8,9}};
+    diagonalSum(arr2,3,3);
+}
