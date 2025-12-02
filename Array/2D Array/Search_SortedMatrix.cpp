bbae9469092de965ca385039d1d2039f2049cbc5
Ujjal Burnwal <ujjwalbur1@gmail.com>
2025-12-03 00:32:20 +0530

    Add some Comment

--- Array/2D Array/Search_SortedMatrix.cpp
+++ Array/2D Array/Search_SortedMatrix.cpp
@@ -6,11 +6,12 @@ int stairCase(int mat[4][4],int n,int m,int key){
     int r=0,c=m-1;
     while(r<n && c>=0){
         if(mat[r][c]== key){
+            cout<<"Cell Found : (" << r <<","<<c<<")"<<endl;
             return true;
         } else if(mat[r][c] < key){
-            r++;
+            r++; //Down
         } else{
-            c--;
+            c--; // Left
         }
     }
     return false;
@@ -48,7 +49,7 @@ int Binarysearch(int arr[][4],int n,int m,int key){
 int main(){
     int arr[4][4] = {{10,20,30,40},{15,25,35,45},{27,29,37,48},{32,33,39,50}};
     if(stairCase(arr,4,4,50)){
-        cout<<"Found" <<endl;
+        cout<<"Completed" <<endl;
     } else {
         cout<<"Not Found"<<endl;
     }
