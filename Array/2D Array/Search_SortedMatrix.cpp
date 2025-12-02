b57d371bfe6c12a7cd2349628ed8614c13f07bcd
Ujjal Burnwal <ujjwalbur1@gmail.com>
2025-12-03 00:02:04 +0530

    We are using Binary search process

--- Array/2D Array/Search_SortedMatrix.cpp
+++ Array/2D Array/Search_SortedMatrix.cpp
@@ -2,18 +2,36 @@
 using namespace std;
 
 int search(int arr[][4],int n,int m,int key){
+    // Using Binary Search Algo
+    int st=0,mid=0,end=n-1;
     for(int i=0;i<n;i++){
-        for(int j=0;j<m;j++){
-            if(arr[i][j] == key)
+        while(st<=end){
+            mid = (st+end) / 2;
+            if(arr[i][mid] == key){
                 return true;
+            } else if(arr[i][mid] < key){
+                st = mid+1;
+            } else{
+                end = mid -1;
+            }
         }
+        st=mid=0;
+        end = n-1;
     }
     return false;
+
+    // for(int i=0;i<n;i++){
+    //     for(int j=0;j<m;j++){
+    //         if(arr[i][j] == key)
+    //             return true;
+    //     }
+    // }
+    // return false;
 }
 
 int main(){
     int arr[4][4] = {{10,20,30,40},{15,25,35,45},{27,29,37,48},{32,33,39,50}};
-    if(search(arr,4,4,2)){
+    if(search(arr,4,4,20)){
         cout<<"Found" <<endl;
     } else {
         cout<<"Not Found"<<endl;
