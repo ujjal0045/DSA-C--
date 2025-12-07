b480f4d89ce219de781c95672411ee522033b320
Ujjal Burnwal <ujjwalbur1@gmail.com>
2025-12-07 15:31:56 +0530

    Learn for each loop

---
+++ Array/char data/for-each.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+    string str = "Hello World";
+
+    for(char ch:str){
+        cout<<ch <<" ";
+    }
+    cout<<endl;
+}
