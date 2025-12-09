1decc25a291162079e826c818d818c9a267388f6
Ujjal Burnwal <ujjwalbur1@gmail.com>
2025-12-09 20:02:20 +0530

    Create a dynamic array using new keyword

---
+++ Array/Dynamic Array/Create.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int size;
+    cin>>size;
+
+    // Dynamic array
+    int *arr = new int[size];
+    for(int i=0;i<size;i++){
+        arr[i] = i;
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+}
