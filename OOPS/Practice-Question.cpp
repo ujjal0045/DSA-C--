f9ffd7f9e1aab3a2e109cf5092881ad13f24c2a6
Ujjal Burnwal <ujjwalbur1@gmail.com>
2025-12-16 01:19:37 +0530

    Solved the practice question

--- OOPS/Practice-Question.cpp
+++ OOPS/Practice-Question.cpp
@@ -3,13 +3,31 @@
 using namespace std;
 
 class User{
-
+private:
+    long long id;
+    string password;
 public:
     string username;
-private:
 
+    User(long long id){
+        this->id = id;
+    }
+
+    // use setter
+    void setPassword(string passwordVal){
+        password = passwordVal;
+    }
+    // use getter to return the value
+    string getPass(){
+        return password;
+    }
 };
 
 int main(){
+    User s1(1);
+    s1.username="Ujjwal 0045X";
+    s1.setPassword("Ujjwal1223");
 
+    cout<<"Username : "<<s1.username<<endl;
+    cout<<"Password : "<<s1.getPass()<<endl;
 }
