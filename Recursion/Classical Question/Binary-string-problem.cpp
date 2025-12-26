fa3f01299666529f6f744cd5dd35098f39d7919b
Ujjal Burnwal <ujjwalbur1@gmail.com>
2025-12-26 20:31:17 +0530

    Binary string problem asking by paytm

---
+++ Recursion/Classical Question/Binary-string-problem.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+void binString(int n,int lastplace,string ans)
+{
+    if(n == 0){
+        cout<<ans<<endl;
+        return;
+    }
+    if(lastplace != 1){
+        binString(n-1,0,ans+'0');
+        binString(n-1,1,ans+'1');
+    } else{
+        binString(n-1,0,ans+'0');
+    }
+}
+
+int main(){
+    string ans = "";
+    binString(5,0,ans);
+    return -1;
+}
