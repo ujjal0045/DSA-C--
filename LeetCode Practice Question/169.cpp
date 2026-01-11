06673932acfdb0021ed6f57d63dd8c797110886a
Ujjal Burnwal <ujjwalbur1@gmail.com>
2026-01-11 18:35:00 +0530

    Write a better approach take O(N) time

--- LeetCode Practice Question/169.cpp
+++ LeetCode Practice Question/169.cpp
@@ -2,11 +2,30 @@
 #include<vector>
 #include<algorithm>
 using namespace std;
-
+// Time complexity = O(Nlogn)
 int majorityElement(vector<int>& nums) {
         sort(nums.begin(),nums.end());
         int n = nums.size()/2;
         return nums[n];
+}
+
+// Time complexity = O(n)
+int majorityElement2(vector<int>& nums) {
+        int count = 0;
+        int candidate = 0;
+
+        for(int num : nums){
+            if(count == 0){
+                candidate = num;
+            }
+            if(num == candidate){
+                count++;
+            }
+            else {
+                count--;
+            }
+        }
+        return candidate;
     }
 
 int main(){
