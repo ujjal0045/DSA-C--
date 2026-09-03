a2601a5ab4e34766e5b8991df9bb5bf3efb0e00e
Ujjal Burnwal <ujjwalbur1@gmail.com>
2026-09-03 16:04:17 +0530

    Refactor Armstrong number calculation to use dynamic power calculation and improve code structure

--- Function/Armstrong-Number.cpp
+++ Function/Armstrong-Number.cpp
@@ -1,11 +1,26 @@
 #include<iostream>
+#include<cmath>
 using namespace std;
+
+
+int countNumber(long long n){
+    if(n == 0){
+        return 1;
+    }
+    int c = 0;
+    while(n != 0){
+        n = n / 10;
+        c++;
+    }
+    return c;
+}
 
 bool isArmstrong(long long n){
     int temp=n,r,sum=0;
+    int c = countNumber(n);
     while(n > 0){
         r = n % 10;
-        sum += (r*r*r);
+        sum += pow(r,c);
         n /= 10; 
     }
     if(temp == sum){
